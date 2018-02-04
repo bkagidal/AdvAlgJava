5323f9317e520634ad745512ccaa10595ba9dff9
Himavarsha Kagidala <himavarshakagidala@Himavarshas-MacBook-Pro.local>
2018-02-03 18:52:12 -0800

    maze

--- src/main/java/com/lab/dfs/maze/App.java
+++ src/main/java/com/lab/dfs/maze/App.java
@@ -1,5 +1,16 @@
 package com.lab.dfs.maze;
 
 public class App {
+	
+	public static void main(String[] args) {
+		
+		FileReader reader = new FileReader("map.txt", 5, 5);
+		
+		reader.parseFile();
+		
+		MazeSolver maze = new MazeSolver(reader.getData(),reader.getStartRowOrdinate(), reader.getStartColOrdinate());
+		
+		maze.findWayOut();
+	}
 
 }
--- src/main/java/com/lab/dfs/maze/FileReader.java
+++ src/main/java/com/lab/dfs/maze/FileReader.java
@@ -1,6 +1,7 @@
 package com.lab.dfs.maze;
 
 import java.io.File;
+import java.util.Scanner;
 
 public class FileReader {
 
@@ -21,6 +22,72 @@ public class FileReader {
 	public void parseFile(){
 		
 		File file = new File(fileName);
-		
+		int ele;
+		try{
+			
+			Scanner scan = new Scanner(file); 
+			for(int i=0;i<noOfRows;i++){
+				for(int j=0;j<noofCols;j++){
+					ele = scan.nextInt();
+					if(ele == 2){
+						startRowOrdinate= i;
+						startColOrdinate = j;
+					}
+					data[i][j] = ele;
+				}
+			}
+			
+			scan.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	public int[][] getData() {
+		return data;
+	}
+
+	public void setData(int[][] data) {
+		this.data = data;
+	}
+
+	public int getStartRowOrdinate() {
+		return startRowOrdinate;
+	}
+
+	public void setStartRowOrdinate(int startRowOrdinate) {
+		this.startRowOrdinate = startRowOrdinate;
+	}
+
+	public int getStartColOrdinate() {
+		return startColOrdinate;
+	}
+
+	public void setStartColOrdinate(int startColOrdinate) {
+		this.startColOrdinate = startColOrdinate;
+	}
+
+	public int getNoOfRows() {
+		return noOfRows;
+	}
+
+	public void setNoOfRows(int noOfRows) {
+		this.noOfRows = noOfRows;
+	}
+
+	public int getNoofCols() {
+		return noofCols;
+	}
+
+	public void setNoofCols(int noofCols) {
+		this.noofCols = noofCols;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
 	}
 }
