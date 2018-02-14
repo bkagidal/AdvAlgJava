874280e3965853ea8a4ca8ce96c6586cef4956ce
Balaraju K Kagidala -X (bkagidal - TECH MAHINDRA LIM at Cisco) <bkagidal@cisco.com>
2018-02-14 12:13:59 -0800

    DAG

--- src/main/java/com/lab/dag/AcyclicShortestPath.java
+++ src/main/java/com/lab/dag/AcyclicShortestPath.java
@@ -1,22 +1,62 @@
 package com.lab.dag;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Stack;
 
 public class AcyclicShortestPath {
-	
-	public void shortestPaths(List<Vertex> vertices,Vertex sourceVertex,Vertex targetVertex){
-		
+
+	public void shortestPaths(List<Vertex> vertices, Vertex sourceVertex, Vertex targetVertex) {
+
 		sourceVertex.setMinDistance(0);
-		
+
 		TopologicalSort top = new TopologicalSort();
 		top.topologicalSort(vertices);
-		
+
 		Stack<Vertex> stack = top.getTopologicalOrder();
-		
-		for(Vertex v : stack){
-			System.out.print( v+" ");
+
+		for (Vertex ver : stack) {
+			//System.out.print(ver + " ");
+
+			for (Edge e : ver.getAdjList()) {
+
+				Vertex u = e.getStartVertex();
+				Vertex v = e.getTargetVertex();
+
+				double newdistance = u.getMinDistance() + e.getWeight();
+
+				if (newdistance < v.getMinDistance()) {
+
+					v.setMinDistance(newdistance);
+					v.setPredecessor(ver);
+				}
+			}
 		}
+
+		//System.out.println("\n");
+		if (targetVertex.getMinDistance() == Double.MAX_VALUE)
+			System.out.println("There no Shortest Path..");
+		else
+			System.out.println("Target Vertex Shortest Path " + targetVertex.getMinDistance());
+	}
+	
+	public void showShortestPath(Vertex targetVer){
+		
+		List<Vertex> list = new ArrayList<Vertex>();
+		
+		Vertex actualVer = targetVer;
+		
+		list.add(actualVer);
+		
+		while(actualVer.getPredecessor() != null){
+			list.add(actualVer.getPredecessor());
+			actualVer = actualVer.getPredecessor();
+		}
+		
+		Collections.reverse(list);
+		
+		System.out.println(list);
 	}
 
 }
--- src/main/java/com/lab/dag/App.java
+++ src/main/java/com/lab/dag/App.java
@@ -1,5 +1,29 @@
 package com.lab.dag;
 
-public class App {
+import java.util.ArrayList;
+import java.util.List;
 
+public class App {
+	public static void main(String[] args) {
+
+		Vertex vertex0 = new Vertex("A");
+		Vertex vertex1 = new Vertex("B");
+		Vertex vertex2 = new Vertex("C");
+
+		List<Vertex> vertexList = new ArrayList<Vertex>();
+
+		vertexList.add(vertex0);
+		vertexList.add(vertex1);
+		vertexList.add(vertex2);
+		
+		vertex0.addNeighbour(new Edge(1, vertex0, vertex1));
+		vertex0.addNeighbour(new Edge(10, vertex0, vertex2));
+
+		vertex1.addNeighbour(new Edge(1, vertex1, vertex2));
+
+
+		AcyclicShortestPath acyclicShortestPath = new AcyclicShortestPath();
+		acyclicShortestPath.shortestPaths(vertexList, vertex0, vertex2);
+		acyclicShortestPath.showShortestPath(vertex2);
+	}
 }
