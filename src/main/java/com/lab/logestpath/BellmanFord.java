208c32907cd99f5e0cb5c003133d6e96059b3a3b
Balaraju K Kagidala -X (bkagidal - TECH MAHINDRA LIM at Cisco) <bkagidal@cisco.com>
2018-02-15 22:22:35 -0800

    longestpath

--- src/main/java/com/lab/logestpath/BellmanFord.java
+++ src/main/java/com/lab/logestpath/BellmanFord.java
@@ -1,5 +1,66 @@
 package com.lab.logestpath;
 
+import java.util.List;
+
 public class BellmanFord {
+
+	private List<Vertex> vertexList;
+	private List<Edge> edgeList;
+
+	public BellmanFord(List<Vertex> verList, List<Edge> edgeList) {
+		this.vertexList = verList;
+		this.edgeList = edgeList;
+	}
+
+	public void bellmanford(Vertex sourceVertex, Vertex targetVertex) {
+
+		sourceVertex.setMinDistance(0);
+
+		for (int i = 0; i < vertexList.size() - 1; i++) {
+
+			for (Edge e : edgeList) {
+
+				if (e.getStartVertex().getMinDistance() == Integer.MAX_VALUE)
+					continue;
+
+				int newdistance = e.getStartVertex().getMinDistance() + e.getWeight();
+
+				if (newdistance < e.getTargetVertex().getMinDistance()) {
+					e.getTargetVertex().setMinDistance(newdistance);
+					e.getTargetVertex().setPredecessor(e.getStartVertex());
+				}
+			}
+
+		}
+
+		for (Edge e : edgeList) {
+
+			if (e.getStartVertex().getMinDistance() != Integer.MAX_VALUE) {
+
+				if (hasCycle(e)) {
+					System.out.println("There is Cycle exists");
+					return;
+				}
+			}
+		}
+		
+		if(targetVertex.getMinDistance() != Integer.MAX_VALUE){
+			System.out.println("There is shortest path from source to target");
+			
+			Vertex actualVertex = targetVertex;
+			
+			while(actualVertex != null){
+				System.out.print(actualVertex+" ");
+				actualVertex = actualVertex.getPredecessor();
+			}
+		}else{
+			System.out.println("There is no path from source to target");
+		}
+		
+	}
+
+	private boolean hasCycle(Edge e) {
+		return e.getStartVertex().getMinDistance() + e.getWeight() < e.getTargetVertex().getMinDistance();
+	}
 
 }
--- src/main/java/com/lab/logestpath/Vertex.java
+++ src/main/java/com/lab/logestpath/Vertex.java
@@ -1,5 +1,59 @@
 package com.lab.logestpath;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Vertex {
+
+	private String name;
+	private List<Edge> adjList;
+	private Vertex predecessor;
+	private int minDistance = Integer.MAX_VALUE;
+
+	public Vertex(String name) {
+		this.name = name;
+		adjList = new ArrayList<Edge>();
+	}
+
+	public void addNeighbour(Edge e) {
+		adjList.add(e);
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Edge> getAdjList() {
+		return adjList;
+	}
+
+	public void setAdjList(List<Edge> adjList) {
+		this.adjList = adjList;
+	}
+
+	public Vertex getPredecessor() {
+		return predecessor;
+	}
+
+	public void setPredecessor(Vertex predecessor) {
+		this.predecessor = predecessor;
+	}
+
+	public int getMinDistance() {
+		return minDistance;
+	}
+
+	public void setMinDistance(int minDistance) {
+		this.minDistance = minDistance;
+	}
 
 }
